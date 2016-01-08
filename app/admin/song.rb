3eb081d1c28b1df5d8526d825d18a3ae27f360e8
Prelang Builder <builder@prelang.com>
2016-01-08 04:20:59 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Song' to allow for create, update

--- app/admin/song.rb
+++ app/admin/song.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Song do
+  permit_params :title, :length
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
